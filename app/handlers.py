2b4ef3c128f15f866f0c8b8ba24f8beb5ddf2792
Stanislav Sazonov <slavk11@yandex.ru>
2025-03-23 23:53:44 +0300

    Added CallbackQuery

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Router, F
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.enums import ChatAction
 
@@ -16,8 +16,9 @@ async def check_router(message: Message):
 async def cmd_start(message: Message):
     await message.bot.send_chat_action(chat_id=message.from_user.id,
                                        action=ChatAction.TYPING)
-    await asyncio.sleep(2)
-    await message.answer('Добро пожаловать!', reply_markup=ReplyKeyboardRemove())
+    await asyncio.sleep(0.5)
+    await message.answer('Добро пожаловать!',
+                         reply_markup=kb.inline_main)
 
 @router.message(F.text == 'Привет!')
 async def hello(message: Message):
@@ -25,7 +26,7 @@ async def hello(message: Message):
 
 @router.message(Command('help'))
 async def cmd_help(message: Message):
-    await message.answer('Пока что бот не умеет нчиего!', reply_markup=kb.inline_main)
+    await message.answer('Пока что бот не умеет ничего!')
 
 @router.message(F.photo)
 async def photo_photo(message: Message):
@@ -41,3 +42,14 @@ async def video_video(message: Message):
 async def sticker_sticker(message: Message):
     file_id = message.sticker.file_id
     await message.answer_sticker(file_id)
+
+@router.callback_query(F.data == 'catalog')
+async def catalog(callback: CallbackQuery):
+    await callback.answer('Это всплывающее окно')
+    await callback.message.edit_text('Выберите категорию', reply_markup=kb.catalog)
+
+@router.callback_query(F.data.startswith('item_'))
+async def item_handler(callback: CallbackQuery):
+    await callback.answer('Вы выбрали товар')
+    await callback.message.edit_text(f'Вы выбрали {callback.data}',
+                                     reply_markup=kb.back)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,5 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
+
+
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Каталог'),
     KeyboardButton(text='Корзина')],
@@ -10,7 +12,25 @@ main = ReplyKeyboardMarkup(keyboard=[
 )
 
 inline_main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Каталог', url='https://telegram.org'),
-    InlineKeyboardButton(text='Корзина', url='https://telegram.org')],
-    [InlineKeyboardButton(text='Контакты', url='https://telegram.org')]
+    [InlineKeyboardButton(text='Каталог', callback_data='catalog'),
+    InlineKeyboardButton(text='Корзина', callback_data='cart'),],
+    [InlineKeyboardButton(text='Контакты', callback_data='contacts')]
+])
+
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Nike', callback_data='item_nike')],
+    [InlineKeyboardButton(text='Adidas', callback_data='item_adidas')],
+    [InlineKeyboardButton(text='Puma', callback_data='item_puma')],
+    [InlineKeyboardButton(text='Reebok', callback_data='item_reebok')],
+    [InlineKeyboardButton(text='Under Armour', callback_data='item_underarmour')],
+    [InlineKeyboardButton(text='New Balance', callback_data='item_newbalance')],
+    [InlineKeyboardButton(text='Asics', callback_data='item_asics')],
+    [InlineKeyboardButton(text='Fila', callback_data='item_fila')],
+    [InlineKeyboardButton(text='Skechers', callback_data='item_skechers')],
+    [InlineKeyboardButton(text='Converse', callback_data='item_converse')]
+])
+
+
+back = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Назад', callback_data='catalog')],
 ])
