20cdc706c60f848c9e8e8865dec975d2d88621f2
Stanislav Sazonov <slavk11@yandex.ru>
2025-03-26 00:52:58 +0300

    Installed Redis to project

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 import os
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
 
 from dotenv import load_dotenv
 from app.handlers import router
 
+import redis.asyncio as aioredis
+
 async def main():
+    redis = await aioredis.from_url(f'redis://localhost:6379/0')
     load_dotenv()
     bot = Bot(token=os.getenv('TG_TOKEN'))
-    dp = Dispatcher()
+    dp = Dispatcher(storage=RedisStorage(redis))
     dp.startup.register(startup)
     dp.shutdown.register(shutdown)
     dp.include_router(router)
